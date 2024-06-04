f6c33282e95e97e326f8ac62a237be6730124915
Ona <olore@atioinc.com>
2024-06-03 23:36:46 -0300

    Activity Main

--- app/src/main/java/com/example/tp3fickleflightgrupo3/MainActivity.kt
+++ app/src/main/java/com/example/tp3fickleflightgrupo3/MainActivity.kt
@@ -2,68 +2,164 @@ package com.example.tp3fickleflightgrupo3
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.view.MenuItem
-import androidx.appcompat.app.ActionBarDrawerToggle
+import android.view.View
 import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.NavGraph
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.example.tp3fickleflightgrupo3.databinding.ActivityMainBinding
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity() {
 
-    private lateinit var drawerLayout: DrawerLayout
-    private lateinit var topAppBar: MaterialToolbar
-    private lateinit var toggle: ActionBarDrawerToggle
-    private lateinit var navView: NavigationView
+    private lateinit var navController: NavController
+
+    private val fragmentsNavigation = setOf(
+        R.id.action_explore,
+        R.id.flightSearchFragment,
+        R.id.action_offers,
+        R.id.profileFragment
+    )
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        drawerLayout = findViewById(R.id.drawer_layout)
-        topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
-        navView = findViewById(R.id.nav_view_drawer)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        setSupportActionBar(topAppBar)
+        setSupportActionBar(binding.contentMainInclude.customToolbar)
 
-        // Set up the drawer toggle
-        toggle = ActionBarDrawerToggle(
-            this,
-            drawerLayout,
-            topAppBar,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close
+        // Obtener el NavController desde el NavHostFragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Inflar el gráfico de navegación
+        navController.setGraph(R.navigation.nav_graph)
+
+        navView = binding.navView
+        val appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = fragmentsNavigation,
+            fallbackOnNavigateUpListener = ::onSupportNavigateUp
         )
 
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
+        //Toolbar config
+        binding.contentMainInclude.customToolbar.setupWithNavController(
+            navController,
+            appBarConfiguration
+        )
+        navView.setupWithNavController(navController)
 
-        navView.setNavigationItemSelectedListener(this)
-    }
+        //NavigationView
+        NavigationUI.setupActionBarWithNavController(this, navController, binding.drawerLayout)
+//        binding.navigationView.setupWithNavController(navController)
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-//            R.id.nav_home -> {
-//                // Handle navigation to Home
-//            }
-//            R.id.nav_profile -> {
-//                // Handle navigation to Profile
-//            }
-//            R.id.nav_settings -> {
-//                // Handle navigation to Settings
-//            }
-            // Add other navigation options here
+        //Comportamiento de fragmentos
+        navController.addOnDestinationChangedListener { _, destination: NavDestination, _ ->
+            when (destination.id) {
+                R.id.action_explore,
+                R.id.flightSearchFragment,
+                R.id.action_offers,
+                R.id.profileFragment
+                -> {
+                    navView.visibility = BottomNavigationView.VISIBLE
+                    binding.contentMainInclude.customToolbar.visibility = MaterialToolbar.VISIBLE
+                }
+                else -> {
+                    navView.visibility = BottomNavigationView.GONE
+                    binding.contentMainInclude.customToolbar.visibility = View.GONE
+                }
+            }
         }
+//        esto no se si va
+//        binding.txtCloseApp.setOnClickListener(View.OnClickListener {
+//            finish()
+//        })
 
-        drawerLayout.closeDrawer(GravityCompat.START)
-        return true
+//        Carga modo oscuro/claro
+//        loadPreferences()
+
     }
 
-    override fun onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START)
+    override fun onSupportNavigateUp(): Boolean {
+        //Fuezo al boton de navegación de la toolbar que solo abra el menú Drawer
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
         } else {
-            super.onBackPressed()
+            binding.drawerLayout.openDrawer(GravityCompat.START)
         }
+        return true;
     }
+
+//    Modo oscuro
+//    private fun loadPreferences() {
+//        val prefs = PreferenceManager.getDefaultSharedPreferences(this);
+//        if (prefs.getBoolean(getString(R.string.preference_night_mode_key), false)) {
+//            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+//        } else {
+//            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+//        }
+//    }
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        drawerLayout = findViewById(R.id.drawer_layout)
+//        topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
+//        navView = findViewById(R.id.nav_view_drawer)
+//
+//        setSupportActionBar(topAppBar)
+//
+//
+//        // Set up the drawer toggle
+//        toggle = ActionBarDrawerToggle(
+//            this,
+//            drawerLayout,
+//            topAppBar,
+//            R.string.navigation_drawer_open,
+//            R.string.navigation_drawer_close
+//        )
+//
+//        drawerLayout.addDrawerListener(toggle)
+//        toggle.syncState()
+//
+//        navView.setNavigationItemSelectedListener(this)
+//    }
+//
+//    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+//        when (item.itemId) {
+////            R.id.nav_home -> {
+////                // Handle navigation to Home
+////            }
+////            R.id.nav_profile -> {
+////                // Handle navigation to Profile
+////            }
+////            R.id.nav_settings -> {
+////                // Handle navigation to Settings
+////            }
+//            // Add other navigation options here
+//        }
+//
+//        drawerLayout.closeDrawer(GravityCompat.START)
+//        return true
+//    }
+//
+//    override fun onBackPressed() {
+//        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+//            drawerLayout.closeDrawer(GravityCompat.START)
+//        } else {
+//            super.onBackPressed()
+//        }
+//    }
 }
